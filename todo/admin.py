67798c874f4a366c472336950b4574ffa7b89db0
Hexbiner <50555076+Hexbiner@users.noreply.github.com>
2019-06-25 12:47:05 +0300

    Update admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from .models import Category, TodoList #Projects , Tasks
+from .models import Category, TodoList, ArchiveList
 
 class TodoListAdmin(admin.ModelAdmin):
     list_display = ("title",  "created", "due_date")
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name",)
+class ArchiveListAdmin(admin.ModelAdmin):
+    list_display = ("title",  "created", "due_date")
 
 admin.site.register(Category)
 admin.site.register(TodoList)
-#admin.site.register(Projects)
-#admin.site.register(Tasks)
+admin.site.register(ArchiveList)
