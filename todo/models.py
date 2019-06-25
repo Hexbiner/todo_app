5c953cfd7474ed513640211ba19ac3149daf6036
Hexbiner <50555076+Hexbiner@users.noreply.github.com>
2019-06-25 12:47:30 +0300

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,21 +1,33 @@
 from django.db import models
 from django.utils import timezone
 
-class Category(models.Model): # The Category table name that inherits models.Model
-    name = models.CharField(max_length=100) #Like a varchar
+class Category(models.Model):
+    name = models.CharField(max_length=100)
     class Meta:
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
-    def __str__(self):
-        return self.name #name to be shown when called
 
-class TodoList(models.Model): #Todolist able name that inherits models.Model
-    title = models.CharField(max_length=250) # a varchar
-    content = models.TextField(blank=True) # a text field
-    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="general") # a foreignkey
-    class Meta:
-        ordering = ["-created"] #ordering by the created field
     def __str__(self):
-        return self.title #name to be shown when called
+        return self.name
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="general")
+    class Meta:
+        ordering = ["-created"]
+    def __str__(self):
+        return self.title
+
+class ArchiveList(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="general")
+    class Meta:
+        ordering = ["-created"]
+    def __str__(self):
+        return self.title
