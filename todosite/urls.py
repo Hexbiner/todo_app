c9b3f8acdb285614c112f4769f70a1153b35583a
Hexbiner <50555076+Hexbiner@users.noreply.github.com>
2019-06-25 12:48:33 +0300

    Update urls.py

--- todosite/urls.py
+++ todosite/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import index
+from todo.views import index, archive
 from todo import views
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^$', index, name="TodoList"),
+    url(r'^archive/', archive, name="ArchiveList"),
 ]
+
+
+#url(r'^accounts/', include('allauth.urls')),
